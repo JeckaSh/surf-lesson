82e4acc8e65879258cdabee89af6ff5473ba4d7f
JeckaSh <jeckashevelkov@icloud.com>
2023-10-18 21:55:12 +0300

    task 7 solution upload

--- task-7/lib/surf_dart_courses_template.dart
+++ task-7/lib/surf_dart_courses_template.dart
@@ -1,60 +1,177 @@
-// task 7
+filterProducts(List<RawProductItem> products) {
+  Map<String, Map<String, List<String>>> result = {};
 
-// Техническое задание
-// В одном еще совсем маленьком, но подающим большие надежды магазине "Шестёрочка" появилось своё IT-отделение. 
-// Одной из задач стало написание своего мобильного приложения с каталогом товаров.
+  for (var product in products) {
+    if (product.expirationDate.isBefore(DateTime.now()) || product.qty == 0) {
+      continue;
+    }
 
-// В силу малого опыта разработчик бэкенда Костя (и по совместительству племянник хозяина магазина) отправляет каталог 
-// товаров одним большим списком. Для удобства пользователя все товары решили отображать по их категориям и подкатегориям. 
-// Вам предстоит решить задачу их фильтрации самостоятельно!
+    if (!result.containsKey(product.categoryName)) {
+      result[product.categoryName] = {};
+    }
 
-// У каждого товара есть:
+    if (!result[product.categoryName]!.containsKey(product.subcategoryName)) {
+      result[product.categoryName]![product.subcategoryName] = [];
+    }
 
-// название;
-// категория;
-// подкатегория;
-// срок годности;
-// количество в наличии.
-// Ваша цель — структурировать товары по подкатегориям, а подкатегории — по категориям. При этом в конечную подборку 
-// не должны попасть товары, которые:
+    result[product.categoryName]![product.subcategoryName]!.add(product.name);
+  }
+  return result;
+}
 
-// уже просрочены;
-// закончились на складе.
-// Подкатегории, содержащие только товары, подпадающие под условия выше, должны отсутствовать в финальной выборке, 
-// равно как и категории, состоящие из таких подкатегорий.
+class RawProductItem {
+  String name;
+  String categoryName;
+  String subcategoryName;
+  DateTime expirationDate;
+  int qty;
 
-// Входные данные
-// В качестве входных данных — список сущностей `RawProductItem, содержащий поля:
+  RawProductItem(
+      {required this.name,
+      required this.categoryName,
+      required this.subcategoryName,
+      required this.expirationDate,
+      required this.qty});
+}
 
-// `name` — название товара;
-// `categoryName` — название категории;
-// `subcategoryName` — название подкатегории;
-// `expirationDate` — срок годности;
-// `qty` — кол-во на складе.
+void main() {
+  List<RawProductItem> products = [
+    RawProductItem(
+      name: 'Персик',
+      categoryName: 'Растительная пища',
+      subcategoryName: 'Фрукты',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 5,
+    ),
+    RawProductItem(
+      name: 'Молоко',
+      categoryName: 'Молочные продукты',
+      subcategoryName: 'Напитки',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 5,
+    ),
+    RawProductItem(
+      name: 'Кефир',
+      categoryName: 'Молочные продукты',
+      subcategoryName: 'Напитки',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 5,
+    ),
+    RawProductItem(
+      name: 'Творог',
+      categoryName: 'Молочные продукты',
+      subcategoryName: 'Не напитки',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 0,
+    ),
+    RawProductItem(
+      name: 'Творожок',
+      categoryName: 'Молочные продукты',
+      subcategoryName: 'Не напитки',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 0,
+    ),
+    RawProductItem(
+      name: 'Творог',
+      categoryName: 'Молочные продукты',
+      subcategoryName: 'Не напитки',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 0,
+    ),
+    RawProductItem(
+      name: 'Гауда',
+      categoryName: 'Молочные продукты',
+      subcategoryName: 'Сыры',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 3,
+    ),
+    RawProductItem(
+      name: 'Маасдам',
+      categoryName: 'Молочные продукты',
+      subcategoryName: 'Сыры',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 2,
+    ),
+    RawProductItem(
+      name: 'Яблоко',
+      categoryName: 'Растительная пища',
+      subcategoryName: 'Фрукты',
+      expirationDate: DateTime(2023, 12, 4),
+      qty: 4,
+    ),
+    RawProductItem(
+      name: 'Морковь',
+      categoryName: 'Растительная пища',
+      subcategoryName: 'Овощи',
+      expirationDate: DateTime(2023, 12, 23),
+      qty: 51,
+    ),
+    RawProductItem(
+      name: 'Черника',
+      categoryName: 'Растительная пища',
+      subcategoryName: 'Ягоды',
+      expirationDate: DateTime(2023, 12, 25),
+      qty: 0,
+    ),
+    RawProductItem(
+      name: 'Курица',
+      categoryName: 'Мясо',
+      subcategoryName: 'Птица',
+      expirationDate: DateTime(2023, 12, 18),
+      qty: 2,
+    ),
+    RawProductItem(
+      name: 'Говядина',
+      categoryName: 'Мясо',
+      subcategoryName: 'Не птица',
+      expirationDate: DateTime(2023, 12, 17),
+      qty: 0,
+    ),
+    RawProductItem(
+      name: 'Телятина',
+      categoryName: 'Мясо',
+      subcategoryName: 'Не птица',
+      expirationDate: DateTime(2023, 12, 17),
+      qty: 0,
+    ),
+    RawProductItem(
+      name: 'Индюшатина',
+      categoryName: 'Мясо',
+      subcategoryName: 'Птица',
+      expirationDate: DateTime(2023, 12, 17),
+      qty: 0,
+    ),
+    RawProductItem(
+      name: 'Утка',
+      categoryName: 'Мясо',
+      subcategoryName: 'Птица',
+      expirationDate: DateTime(2023, 12, 18),
+      qty: 0,
+    ),
+    RawProductItem(
+      name: 'Гречка',
+      categoryName: 'Растительная пища',
+      subcategoryName: 'Крупы',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 8,
+    ),
+    RawProductItem(
+      name: 'Свинина',
+      categoryName: 'Мясо',
+      subcategoryName: 'Не птица',
+      expirationDate: DateTime(2023, 12, 23),
+      qty: 5,
+    ),
+    RawProductItem(
+      name: 'Груша',
+      categoryName: 'Растительная пища',
+      subcategoryName: 'Фрукты',
+      expirationDate: DateTime(2023, 12, 25),
+      qty: 5,
+    ),
+  ];
 
-// Выходные данные
-// Выходные данные представлены словарем (мапой), где:
+  var filteredProducts = filterProducts(products);
 
-// в роли ключа — название категории типа `String`;
-// в роли значения — словарь подкатегорий, которые входят в данную категорию.
-// В свою очередь для мапы подкатегорий:
-
-// в роли ключа — название подкатегории типа `String`;
-// в роли значения — список названий товаров, входящих в подкатегорию, являющуюся ключом.
-// Пример выходных данных:
-
-// {
-//   'Растительная пища': {
-//     'Фрукты': ['Персик', 'Груша'],
-//     'Овощи': ['Морковь'],
-//     'Крупы': ['Гречка'],
-//   },
-//   'Молочные продукты': {
-//     'Напитки': ['Молоко', 'Кефир'],
-//     'Сыры': ['Гауда', 'Маасдам'],
-//   },
-//   'Мясо': {
-//     'Птица': ['Курица'],
-//     'Не птица': ['Свинина'],
-//   },
-// }
+  print(filteredProducts);
+}
